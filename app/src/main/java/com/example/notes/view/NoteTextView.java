677932b2f6dd377475848f68717d44b2fbf476f5
YannikKintscher <playeryannik@gmai.com>
2020-11-11 16:45:40 +0100

    Designed Note view in portrait format. No functionality

---
+++ app/src/main/java/com/example/notes/view/NoteTextView.java
@@ -0,0 +1,16 @@
+package com.example.notes.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.notes.R;
+
+public class NoteTextView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_text_view);
+    }
+}
