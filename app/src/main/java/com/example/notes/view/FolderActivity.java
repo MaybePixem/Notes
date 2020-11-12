fee4d5d5ca8c596dfd25b671d630057a706a9edb
Tim <tim712d@protonmail.ch>
2020-11-12 15:42:49 +0100

    implemented search function

--- app/src/main/java/com/example/notes/view/FolderActivity.java
+++ app/src/main/java/com/example/notes/view/FolderActivity.java
@@ -8,8 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SearchView;
 
 import com.example.notes.R;
 import com.example.notes.data.FolderDAO;
@@ -20,6 +23,7 @@ import com.orm.SchemaGenerator;
 import com.orm.SugarContext;
 import com.orm.SugarDb;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FolderActivity extends AppCompatActivity {
@@ -47,22 +51,48 @@ public class FolderActivity extends AppCompatActivity {
 
         final FloatingActionButton createNewNoteBtn = findViewById(R.id.createNewNoteBtn);
         createNewNoteBtn.setOnClickListener(v -> handleCreateNewNoteBtnOnClick());
+
+        SearchView searchView = findViewById(R.id.searchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                updateFolderList(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                updateFolderList(newText);
+                return true;
+            }
+        });
+
         displayList(allFolders);
+    }
+
+    private void updateFolderList(String query) {
+        List<Folder> newFolderList = new ArrayList<>();
+        for (Folder folder : folderDAO.getAllFolders()) {
+            if (folder.getName().contains(query)) {
+                newFolderList.add(folder);
+            }
+        }
+        displayList(newFolderList);
     }
 
 
     private void displayList(List<Folder> folderList) {
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        FolderListAdapter folderListAdapter = new FolderListAdapter(folderList);
-        recyclerView.setAdapter(folderListAdapter);
+        if (editFlag) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            FolderEditListAdapter folderEditListAdapter = new FolderEditListAdapter(folderList);
+            recyclerView.setAdapter(folderEditListAdapter);
+        } else {
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            FolderListAdapter folderListAdapter = new FolderListAdapter(folderList);
+            recyclerView.setAdapter(folderListAdapter);
+        }
     }
-
-    private void displayEditList(List<Folder> folderList) {
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        FolderEditListAdapter folderEditListAdapter = new FolderEditListAdapter(folderList);
-        recyclerView.setAdapter(folderEditListAdapter);
-    }
-
 
     private void handleCreateNewNoteBtnOnClick() {
     }
@@ -80,11 +110,7 @@ public class FolderActivity extends AppCompatActivity {
             Folder folder = new Folder(input.getText().toString());
             folder.save();
             List<Folder> allFolders = folderDAO.getAllFolders();
-            if (editFlag) {
-                displayEditList(allFolders);
-            } else {
-                displayList(allFolders);
-            }
+            displayList(allFolders);
         });
         builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
         builder.show();
@@ -92,14 +118,13 @@ public class FolderActivity extends AppCompatActivity {
 
     private void handleEditBtnOnClick() {
         if (editFlag) {
-            displayList(folderDAO.getAllFolders());
             editBtn.setText("Bearbeiten");
             editFlag = false;
         } else {
-            displayEditList(folderDAO.getAllFolders());
             editBtn.setText("Fertig");
             editFlag = true;
         }
+        displayList(folderDAO.getAllFolders());
     }
 
     @Override
--- app/src/main/java/com/example/notes/view/NotesActivity.java
+++ app/src/main/java/com/example/notes/view/NotesActivity.java
@@ -6,7 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.example.notes.R;
@@ -16,6 +18,7 @@ import com.example.notes.model.Note;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.orm.SugarContext;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NotesActivity extends AppCompatActivity {
@@ -47,28 +50,86 @@ public class NotesActivity extends AppCompatActivity {
         final FloatingActionButton createNewNoteBtn = findViewById(R.id.createNewNoteBtn);
         createNewNoteBtn.setOnClickListener(v -> handleCreateNewNoteBtnOnClick());
 
+        SearchView searchView = findViewById(R.id.searchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                updateNoteList(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                updateNoteList(newText);
+                return true;
+            }
+        });
+
+        displayList(noteList);
+    }
+
+    private void updateNoteList(String query) {
+        List<Note> noteList = new ArrayList<>();
+        for (Note note : noteDAO.getAllNotesFromFolder(folderId)) {
+            if (note.getTitle().contains(query)) {
+                noteList.add(note);
+            }
+        }
         displayList(noteList);
     }
 
     private void displayList(List<Note> noteList) {
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        NoteListAdapter noteListAdapter = new NoteListAdapter(noteList);
-        recyclerView.setAdapter(noteListAdapter);
+        if (editFlag) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            NoteEditListAdapter noteEditListAdapter = new NoteEditListAdapter(noteList);
+            recyclerView.setAdapter(noteEditListAdapter);
+        } else {
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            NoteListAdapter noteListAdapter = new NoteListAdapter(noteList);
+            recyclerView.setAdapter(noteListAdapter);
+        }
     }
 
-    private void displayEditList(List<Note> noteList) {
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        NoteEditListAdapter noteEditListAdapter = new NoteEditListAdapter(noteList);
-        recyclerView.setAdapter(noteEditListAdapter);
-    }
 
     private void handleEditBtnOnClick() {
+        if (editFlag) {
+            editBtn.setText("Bearbeiten");
+            editFlag = false;
+        } else {
+            editBtn.setText("Fertig");
+            editFlag = true;
+        }
+        displayList(noteDAO.getAllNotesFromFolder(folderId));
     }
 
     private void handleCreateNewNoteBtnOnClick() {
         Intent intent = new Intent(this, NoteTextView.class);
-        intent.putExtra("noteId", 1);
         intent.putExtra("folderId", folderId);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        navigateBackToFolders();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: // the default resource ID of the actionBar's back button
+                navigateBackToFolders();
+                break;
+        }
+
+        return true;
+    }
+
+    private void navigateBackToFolders() {
+        Intent intent = new Intent(this, FolderActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
