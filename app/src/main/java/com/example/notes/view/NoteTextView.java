131775d07ad31c57bdf6576a76d862cbba986c64
Tim <tim712d@protonmail.ch>
2020-11-12 12:04:19 +0100

    updated activity_note_text_view.xml

--- app/src/main/java/com/example/notes/view/NoteTextView.java
+++ app/src/main/java/com/example/notes/view/NoteTextView.java
@@ -2,9 +2,11 @@ package com.example.notes.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.notes.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NoteTextView extends AppCompatActivity {
 
@@ -12,5 +14,9 @@ public class NoteTextView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_text_view);
+
+
     }
+
+
 }
--- app/src/main/java/com/example/notes/view/NotesView.java
+++ app/src/main/java/com/example/notes/view/NotesView.java
@@ -2,10 +2,12 @@ package com.example.notes.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.notes.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NotesView extends AppCompatActivity {
 
@@ -15,5 +17,14 @@ public class NotesView extends AppCompatActivity {
         setContentView(R.layout.activity_notes_view);
         TextView folderNameView = findViewById(R.id.folderName);
         folderNameView.setText(getIntent().getStringExtra("folderId"));
+
+        final FloatingActionButton createNewNoteBtn = findViewById(R.id.createNewNoteBtn);
+        createNewNoteBtn.setOnClickListener(v -> handleCreateNewNoteBtnOnClick());
+    }
+
+    private void handleCreateNewNoteBtnOnClick() {
+        Intent intent = new Intent(this, NoteTextView.class);
+        intent.putExtra("noteId", 1);
+        startActivity(intent);
     }
 }
